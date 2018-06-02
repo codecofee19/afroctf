300e96bcb0417f40798fc4056a158ee12980e0d2
Preston Pham <ppham@cloudflare.com>
2018-06-02 16:52:35 -0500

    styling

--- two.js
+++ two.js
@@ -3,14 +3,10 @@ const $ = q => [...document.querySelectorAll(q)];
 function check(e) {
   e.preventDefault();
   if ($('#ipaddr')[0].value === '162.89.4.49') {
-    alert('You got it!');
-    $('.hidden')[0].classList.remove('hidden');
+    $('.hidden').forEach(el => el.classList.remove('hidden'));
   } else {
     alert('Try again');
   }
 }
 
 $('form')[0].addEventListener('submit', check);
-$('#next')[0].addEventListener('click', () => {
-    document.location = "three.html"
-});
